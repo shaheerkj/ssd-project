c0d82d4f5a680eda58ab4cba14be46a0007ca3d7
shaheer <135412019+shaheerkj@users.noreply.github.com>
2026-04-09 17:24:38 +0000

    fix: again

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "change-this-in-production-use-env-var"
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or \
-        "sqlite:///" + os.path.join(BASE_DIR, "vault.db")
+        "sqlite:///" + os.path.join(BASE_DIR, "data", "vault.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # CSRF protection
